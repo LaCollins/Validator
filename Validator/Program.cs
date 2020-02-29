d277e3f485f3d5dc5ff28d78dccf30047ded267c
Laura Collins <laura.e.collins1@gmail.com>
2020-02-29 10:24:04 -0600

    done with power rangers

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -9,12 +9,13 @@ namespace Validator
         {
             Regex ValidEmailRegex = CreateValidEmailRegex();
             int parsedValue; // used to hold out variable
-            string mockingText;
             bool lowerOdds = false;
             bool lowerEvens = false;
             bool upperOdds = false;
             bool upperEvens = false;
             var isMocking = false;
+
+            //Validate a pin
 
             Console.WriteLine("Please enter a pin that is between 4-8 numbers long...");
             var pinCode = Console.ReadLine();
@@ -24,16 +25,22 @@ namespace Validator
             else
                 Console.WriteLine("Invalid Pin");
 
+            //Validate a phone number
+
             Console.WriteLine("Please enter a phone number...");
             var phoneNumber = Console.ReadLine();
             Console.WriteLine("{0}correctly entered", IsPhoneNumber(phoneNumber) ? "" : "in");
+
+            //Validate an e-mail
 
             Console.WriteLine("Please enter an e-mail address...");
             var email = Console.ReadLine();
             Console.WriteLine("{0}correctly entered", ValidEmailRegex.IsMatch(email) ? "" : "in");
 
+            //Validate spongebop style mockery
+
             Console.WriteLine("Say something...");
-            mockingText = Console.ReadLine();
+            var mockingText = Console.ReadLine();
 
             mockingText = mockingText.Replace(" ", "");
 
@@ -58,7 +65,7 @@ namespace Validator
                 else
                 {
                     isMocking = false;
-                }
+                }                
             }
 
             if(isMocking == true)
@@ -70,10 +77,55 @@ namespace Validator
                 Console.WriteLine("At least you're not mocking me.");
             }
 
+            //Validate a power ranger
+            Console.WriteLine("Name an original power ranger...");
+            var rangerName = Console.ReadLine();
+
+            switch (rangerName)
+            {
+                case "Jason Lee Scott":
+                    Console.WriteLine("Correct, he was the Red Ranger");
+                    break;
+                case "Geki":
+                    Console.WriteLine("Correct, he was the Red Ranger");
+                    break;
+                case "Zack Taylor":
+                    Console.WriteLine("Correct, he was the Black Ranger");
+                    break;
+                case "Goushi":
+                    Console.WriteLine("Correct, he was the Black Ranger");
+                    break;
+                case "Trini Kwan":
+                    Console.WriteLine("Correct, she was the Yellow Ranger");
+                    break;
+                case "Boi":
+                    Console.WriteLine("Correct, he was the Yellow Ranger");
+                    break;
+                case "Kimberly Hart":
+                    Console.WriteLine("Correct, she was the Pink Ranger");
+                    break;
+                case "Mei":
+                    Console.WriteLine("Correct, she was the Pink Ranger");
+                    break;
+                case "Dan":
+                    Console.WriteLine("Correct, he was the Blue Ranger");
+                    break;
+                case "Burai":
+                    Console.WriteLine("Correct, he was the Green Ranger");
+                    break;
+                case "Tommy Oliver":
+                    Console.WriteLine("Correct, he was the Green Ranger");
+                    break;
+                default:
+                    Console.WriteLine("That is not a Power Ranger.");
+                    break;
+            }
+
             Console.ReadKey();
         }
 
-        public static bool IsPhoneNumber(string number)
+
+        public static bool IsPhoneNumber(string number) //Regex for checking valid phone number
         {
             string numOnly = Regex.Replace(number, @"[^0-9]+", "");
             if (numOnly.Length == 10 && numOnly.Substring(0, 3) != "555")
@@ -82,7 +134,7 @@ namespace Validator
                 return false;
         }
 
-        private static Regex CreateValidEmailRegex()
+        private static Regex CreateValidEmailRegex() //Regex for checking valid e-mail
         {
             string validEmailPattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
             + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
